5873423a45185d2fa1ab368876b4fd5655bc9cf2
Robert Hatcher <Robhatcher14@gmail.com>
2024-11-06 12:57:18 -0600

    P11 Submission 1

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,20 +1,21 @@
 import "./ItemModal.css";
 import closebtn from "../../assets/closeicon.png";
 
-function ItemModal({ activeModal, onClose, card }) {
-    return (
-  <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
-    <div className="modal__content modal__content_type_image">
-      <button onClick={onClose} className="modal__close" type="button">
-        <img src={closebtn} alt="close" className="modal__close-icon" />
-      </button>
-      <img src={card.link} alt="clothes image" className="modal__image" />
-      <div className="modal__footer">
-        <h2 className="modal__caption">{card.name}</h2>
-        <p className="modal__weather">Weather: {card.weather}</p>
+function ItemModal({ activeModal, onClose, card, onDelete }) {
+  return (
+    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+      <div className="modal__content modal__content_type_image">
+        <button onClick={onClose} className="modal__close" type="button">
+          <img src={closebtn} alt="close" className="modal__close-icon" />
+        </button>
+        <img src={card.imageUrl} alt="clothes image" className="modal__image" />
+        <div className="modal__footer">
+          <h2 className="modal__caption">{card.name}</h2>
+          <p className="modal__weather">Weather: {card.weather}</p>
+        </div>
+        <button className="modal__delete-item-button" onClick={onDelete}>Delete</button>
       </div>
     </div>
-  </div>
   );
 }
 
