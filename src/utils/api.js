8e0411cee3989696b6a01e6129e2fea36b8bc88e
Robert Hatcher <Robhatcher14@gmail.com>
2025-02-08 12:09:44 -0600

    P14 Submit 2

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,10 +3,12 @@ const headers = {
   "Content-Type": "application/json",
 };
 
-const authorization = {
-  "Content-Type": "application/json",
-  authorization: `Bearer ${localStorage.getItem("jwt")}`,
-};
+const authorization = () => {
+  return {
+    "Content-Type": "application/json",
+    authorization: `Bearer ${localStorage.getItem("jwt")}`,
+  }
+}
 
 function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
@@ -26,7 +28,7 @@ function getClothingItems() {
 function addClothingItem({ _id, name, imageUrl, weather }) {
   return request(`${baseUrl}/items`, {
     method: "POST",
-    headers: authorization,
+    headers: authorization(),
     body: JSON.stringify({
       _id,
       name,
@@ -39,14 +41,14 @@ function addClothingItem({ _id, name, imageUrl, weather }) {
 function deleteClothingItem(cardId) {
   return request(`${baseUrl}/items/${cardId}`, {
     method: "DELETE",
-    headers: authorization,
+    headers: authorization(),
   });
 }
 
 function editProfile(user) {
   return request(`${baseUrl}/users/me`, {
     method: "PATCH",
-    headers: authorization,
+    headers: authorization(),
     body: JSON.stringify({
       name: user.name,
       avatar: user.avatar,
@@ -57,14 +59,14 @@ function editProfile(user) {
 function addCardLike(id) {
   return request(`${baseUrl}/items/${id}/likes`, {
     method: "PUT",
-    headers: authorization,
+    headers: authorization(),
   });
 }
 
 function removeCardLike(id) {
   return request(`${baseUrl}/items/${id}/likes`, {
     method: "DELETE",
-    headers: authorization,
+    headers: authorization(),
   });
 }
 
